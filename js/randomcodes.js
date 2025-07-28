2c2c6f821108d16e41a94e8d756d99d62c9d4ba5
Zander Wang <me@zander.wang>
2025-07-27 22:11:46 -0700

    add js for btn active/disable

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,6 +1,9 @@
+var code = "";
+var getCode = " ";
+var btnvalue;
+var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+
 function generateCode() {
-    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
-    var code = '';
     for (let i = 1; i <= 8; i++) {
         var char = Math.random() * str.length;
         code += str.charAt(char);
@@ -10,8 +13,15 @@ function generateCode() {
 
 document.getElementById("codes").innerText = generateCode();
 
-function disableButton() {
-    document.getElementById("submit").disabled = true;
+function disableButton(btnvalue) {
+    document.getElementById("submit").disabled = btnvalue;
+    if (btnvalue) {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
 }
 
 disableButton();
