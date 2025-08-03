ae7bba6c3bbfe719248094d99ad70b0a18c9c7c1
Zander Wang <me@zander.wang>
2025-08-03 15:25:36 -0700

    add filter and cursor

--- js/activity.js
+++ js/activity.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-    $("tbody > tr > td:not(:first-child)").click(function() {
+    $("tbody > tr > td:not(:first-child)").filter(function() {
+        return $(this).text() !== "Not Available";
+    })
+    .addClass("selectable")
+    .click(function() {
       $(this).toggleClass("bg-green");
     })
 });
